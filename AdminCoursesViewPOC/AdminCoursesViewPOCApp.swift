1c35464a47f21e9d12040651b5fc7909b980b281
GuruMahan <iosdeve2022@gmail.com>
2023-02-03 18:19:04 +0530

    Initial Commit

---
+++ AdminCoursesViewPOC/AdminCoursesViewPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdminCoursesViewPOCApp.swift
+//  AdminCoursesViewPOC
+//
+//  Created by Guru Mahan on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AdminCoursesViewPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
